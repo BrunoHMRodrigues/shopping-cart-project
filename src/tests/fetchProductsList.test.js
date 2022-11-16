948225f5f7d03027c09caad2e29843c04c359a76
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-16 15:01:33 -0300

    Realizado Requisito Obrigatório 01

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,25 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalledTimes(1);
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    const API = 'https://api.mercadolibre.com/sites/MLB/search?q=computador';
+    expect(fetch).toHaveBeenCalledWith(API);
   });
 
-  // it('...', () => {
-  // });
+  it('Retorno da função fetchProductsList("computador") é igual a estrutura de dados do objeto computadorSearch', () => {
+    expect(fetchProductsList('computador')).toEqual(computadorSearch);
+  });
+
+  it('Retorno da função fetchProductsList() retorna um erro com a mensagem "Termo de busca não informado"', () => {
+    const msgErro = 'Termo de busca não informado';
+    expect( () => { fetchProductsList() }).toThrow(msgErro);
+  });
 });
