52cbc8abdbb9d00a41a98a14c27beae1a9f636ed
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-16 17:35:30 -0300

    Realizado Requisito Obrigatório 03

--- src/main.js
+++ src/main.js
@@ -1,4 +1,18 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const sectionProducts = document.querySelector('.products');
+
+const data = await fetchProductsList('computador');
+data.forEach((element) => {
+  const product = createProductElement(element);
+  sectionProducts.appendChild(product);
+});
+
+// data.forEach((element) => {
+//   createProductElement(element);
+// });
