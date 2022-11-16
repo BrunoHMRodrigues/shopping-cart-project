97c7ef37de17eb512e38e6bda036d34a959b17de
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-16 20:43:56 -0300

    Realizado Requisito 09

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import { searchCep } from './helpers/cepFunctions';
 import { fetchProductsList, fetchProduct } from './helpers/fetchFunctions';
 import { createProductElement, createCartProductElement } from './helpers/shopFunctions';
-import { saveCartID } from './helpers/cartFunctions';
+import { saveCartID, getSavedCartIDs } from './helpers/cartFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
@@ -12,6 +12,18 @@ const loadingElement = document.createElement('div');
 loadingElement.classList.add('loading');
 loadingElement.innerHTML = 'carregando...';
 sectionProducts.appendChild(loadingElement);
+
+const cart = document.querySelector('.cart__products');
+
+async function addToCart(idProduct) {
+//   console.log(idProduct);
+  const dataProduct = await fetchProduct(idProduct);
+  //   console.log('id: ' + dataProduct.id);
+  const productToCart = createCartProductElement(dataProduct);
+  return productToCart;
+//   cart.appendChild(productToCart);
+}
+
 try {
   const data = await fetchProductsList('computador');
 
@@ -22,6 +34,16 @@ try {
     const product = createProductElement(element);
     sectionProducts.appendChild(product);
   });
+  const savedCartProducts = getSavedCartIDs();
+  //   console.log('saved: ' + savedCartProducts);
+  Promise.all(savedCartProducts.map((idProduct) => addToCart(idProduct)))
+    .then((values) => {
+      values.forEach((element) => cart.appendChild(element));
+    });
+
+//   savedCartProducts.forEach(async (idProduct) => {
+//     await addToCart(idProduct);
+//   });
 } catch (error) {
   const errorElement = document.createElement('div');
   const msgError = 'Algum erro ocorreu, recarregue a página e tente novamente';
@@ -31,15 +53,17 @@ try {
 }
 
 // const buttonsAddCart = document.querySelectorAll('.product__add');
-const cart = document.querySelector('.cart__products');
 
 sectionProducts.addEventListener('click', async (event) => {
   if (event.target.className === 'product__add') {
     const targetElement = event.target;
     const idElement = targetElement.parentNode.firstChild.innerText;
     saveCartID(idElement);
-    const dataProduct = await fetchProduct(idElement);
-    const productToCart = createCartProductElement(dataProduct);
-    cart.appendChild(productToCart);
+    // const dataProduct = await fetchProduct(idElement);
+    // console.log(dataProduct);
+    // console.log('idelement: ' + idElement);
+    const getProductData = await addToCart(idElement);
+    // console.log('getProductData: ' + getProductData);
+    cart.appendChild(getProductData);
   }
 });
