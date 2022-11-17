773197ee35a8436d2fc6273336406f9cc81d6d23
Bruno Rodrigues <brunohmrodrigues1@hotmail.com>
2022-11-17 18:27:57 -0300

    Realizado Requisito Obrigatório 11

--- src/helpers/cepFunctions.js
+++ src/helpers/cepFunctions.js
@@ -3,40 +3,20 @@ export const getAddress = async (CEP) => {
   const API2 = `https://brasilapi.com.br/api/cep/v2/${CEP}`;
   const promises = [fetch(API1), fetch(API2)];
   try {
-    const response = await Promise.any(promises);
-    // console.log(response);
-    const result = await response.json();
-    console.log(result, 'RESULT');
+    const result = await Promise.any(promises).then((response) => response.json());
     return result;
   } catch (error) {
-    console.log(error, 'ERROOOOOOOO');
+    return error;
   }
-
-  // console.log(result);
-  // const responseCEP1 = await fetch(API1);
-  // const data1 = await responseCEP1.json();
-  // return data1;
-  // const responseCEP2 = await fetch(API2);
-  // const data2 = await responseCEP1.json();
-  // const promises = [CEP1, CEP2];
-  // const result = await Promise.any(promises).catch(() => 'CEP não encontrado');
 };
 
 export const searchCep = async (CEP) => {
   const data = await getAddress(CEP);
-  console.log(data, 'endereço');
-  // if (data) {
-  const address = data.address ? data.address : data.street;
-  const district = data.district ? data.district : data.neighborhood;
-  const { city, state } = data;
-  const result = `${address} - ${district} - ${city} - ${state}`;
-  // return result;
-  // }
-  // const error = 'CEP não encontrado';
-  // console.log(error, 'ERROR');
-  if (!address || !district || !city || !state) {
-    const error = 'CEP não encontrado';
-    return error;
+  if (!data.status) {
+    const address = data.address ? data.address : data.street;
+    const district = data.district ? data.district : data.neighborhood;
+    const { city, state } = data;
+    const result = `${address} - ${district} - ${city} - ${state}`;
+    return result;
   }
-  return result;
 };
--- src/main.js
+++ src/main.js
@@ -7,18 +7,8 @@ import './style.css';
 document.querySelector('.cep-button').addEventListener('click', searchCep);
 
 const sectionProducts = document.querySelector('.products');
-const totalValue = document.querySelector('.total-price');
-const cartContainer = document.querySelector('.cart__products');
-const btnCEP = document.querySelector('.cep-button');
-const cepInput = document.querySelector('.cep-input');
 const cart = document.querySelector('.cart__products');
-
-// const loadingElement = document.createElement('div');
-// loadingElement.classList.add('loading');
-// loadingElement.innerHTML = 'carregando...';
-// sectionProducts.appendChild(loadingElement);
-
-// const cart = document.querySelector('.cart__products');
+const totalValue = document.querySelector('.total-price');
 
 async function addToCart(idProduct) {
   const dataProduct = await fetchProduct(idProduct);
@@ -66,54 +56,44 @@ try {
   sectionProducts.appendChild(errorElement);
 }
 
-// const buttonsAddCart = document.querySelectorAll('.product__add');
-// const totalValue = document.querySelector('.total-price');
-// const cartContainer = document.querySelector('.cart__products');
-
 sectionProducts.addEventListener('click', async (event) => {
   if (event.target.className === 'product__add') {
     const targetElement = event.target;
     const idElement = targetElement.parentNode.firstChild.innerText;
     saveCartID(idElement);
-    cartContainer.innerHTML = '';
+    cart.innerHTML = '';
     totalValue.innerHTML = 0;
     getSavedCart();
-    // // const dataProduct = await fetchProduct(idElement);
-    // // console.log(dataProduct);
-    // // console.log('idelement: ' + idElement);
-    // const getProductData = await addToCart(idElement);
-    // // console.log('getProductData: ' + getProductData[0]);
-    // // totalValue.innerHTML = Number(totalValue.innerHTML) + 10;
-    // const lengthInitial = cart.children.length;
-    // cart.appendChild(getProductData);
-    // const lengthAfter = cart.children.length;
-    // if (lengthAfter > lengthInitial) {
-    //   // console.log(Number(getProductData.children[1].children[1].children[0].innerHTML));
-    //   const value = Number(getProductData.children[1].children[1].children[0].innerHTML);
-    //   // console.log('actual: ' + value);
-    //   totalValue.innerHTML = Number(totalValue.innerHTML) + value;
-    // }
   }
 });
 
-cartContainer.addEventListener('click', () => {
-  cartContainer.innerHTML = '';
+cart.addEventListener('click', () => {
+  cart.innerHTML = '';
   totalValue.innerHTML = 0;
   getSavedCart();
 });
 
-// const btnCEP = document.querySelector('.cep-button');
-// const cepInput = document.querySelector('.cep-input');
+const btnCEP = document.querySelector('.cep-button');
+const cepInput = document.querySelector('.cep-input');
 
 btnCEP.addEventListener('click', async () => {
+  const spanAddress = document.querySelector('.cart__address');
   const cepValue = cepInput.value;
   console.log(cepValue);
   const lengthCEP = 8;
+  if (!Number(cepValue)) {
+    const errorMessage = 'CEP não encontrado';
+    spanAddress.innerHTML = errorMessage;
+    return;
+  }
   if (cepValue.length === lengthCEP) {
-    // const CEP = document.querySelector('.cep-input');
     const result = await searchCep(cepValue);
-    console.log(result, 'RESULT FINAL');
-    const spanAddress = document.querySelector('.cart__address');
-    spanAddress.innerHTML = result;
+
+    if (!result) {
+      const errorMessage = 'CEP não encontrado';
+      spanAddress.innerHTML = errorMessage;
+    } else {
+      spanAddress.innerHTML = result;
+    }
   }
 });
